b96dd238aa8a08a374bddeec3721a26e43eaaf0b
christopher.grant@protonmail.com <christopher.grant@protonmail.com>
2022-06-02 13:36:50 +0000

    fix approval notebook

--- approve_code.py
+++ approve_code.py
@@ -5,15 +5,15 @@ destination = f"{bucket}/control/events"
 # COMMAND ----------
 
 from backend import approvals
-approvals.display_widgets(dbutils)
+approvals.display_widgets(spark, dbutils)
 data = approvals.get_widget_values(dbutils)
 
 # COMMAND ----------
 
-validated_data = approvals.validate_widget_values(data)
-enriched_data = approvals.enrich_widget_values(validated_data)
+validated_data = approvals.validate_widget_values(data, dbutils, spark)
+enriched_data = approvals.enrich_widget_values(validated_data, spark)
 approvals.form_event_and_send_to_control(enriched_data, destination, dbutils, spark) and displayHTML("<h1>Request sent.</h1>")
 
 # COMMAND ----------
 
-dbutils.widgets.
+
