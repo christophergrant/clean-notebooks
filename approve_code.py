67b7cfa703271f3d7a228dccea86353117d437b2
christopher.grant@protonmail.com <christopher.grant@protonmail.com>
2022-06-02 13:16:01 +0000

    onboarding, approval changes

---
+++ approve_code.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+bucket = ""
+destination = f"{bucket}/control/events"
+
+# COMMAND ----------
+
+from cleanroom.notebooks import approvals
+approvals.display_widgets(dbutils)
+data = approvals.get_widget_values(dbutils)
+
+# COMMAND ----------
+
+validated_data = approvals.validate_widget_values(data)
+enriched_data = approvals.enrich_widget_values(validated_data)
+approvals.form_event_and_send_to_control(enriched_data, destination, dbutils, spark) and displayHTML("<h1>Request sent.</h1>")
+
+# COMMAND ----------
+
+
