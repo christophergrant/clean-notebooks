19b552c6f7ee600194b082d5831e23b23dd2033e
chris.grant <chris.grant@databricks.com>
2022-06-02 06:34:48 -0700

    change widget_names

--- backend/approvals.py
+++ backend/approvals.py
@@ -3,12 +3,12 @@ from backend import common
 # display_widgets
 def display_widgets(spark, dbutils):
     dbutils.widgets.removeAll()
-    dbutils.widgets.dropdown(name="01_action", defaultValue="REVIEWING", choices=['REVIEWING', 'APPROVE', 'REJECT'])
-    dbutils.widgets.text(name="02_code_name", defaultValue="")
-    dbutils.widgets.text(name="03_reason", defaultValue="")
+    dbutils.widgets.dropdown(name="00_action", defaultValue="REVIEWING", choices=['REVIEWING', 'APPROVE', 'REJECT'])
+    dbutils.widgets.text(name="01_code_name", defaultValue="")
+    dbutils.widgets.text(name="02_reason", defaultValue="")
 
 def get_widget_values(dbutils):
-    widget_names = ["action", "description", "action"]
+    widget_names = ["action", "code_name", "reason"]
     return common.get_widget_values(widget_names, dbutils)
 
 def validate_widget_values(d, dbutils, spark):
