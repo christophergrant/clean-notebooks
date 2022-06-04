bd0c11b900cd455550b8ad336ac2b4ed1a0a5d8c
craig-db <craig-db>
2022-06-04 18:17:37 +0000

    removing debugging code

--- create_job.py
+++ create_job.py
@@ -9,13 +9,3 @@ dbutils.widgets.removeAll()
 # COMMAND ----------
 
 jobs.display_widgets(spark, dbutils)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC 
-# MAGIC select * from control.events order by timestamp desc limit 30
-
-# COMMAND ----------
-
-
