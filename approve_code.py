5ef987da6c080d3f6334a7d83922e18d451413f2
craig-db <craig-db>
2022-06-05 15:52:27 +0000

    per conversation

--- approve_code.py
+++ approve_code.py
@@ -3,17 +3,26 @@ dbutils.widgets.removeAll()
 
 # COMMAND ----------
 
+from backend import approvals
+data = None
 try:
     data = approvals.get_widget_values(dbutils)
-    validated_data = approvals.validate_widget_values(data, dbutils, spark)
-    enriched_data = approvals.enrich_widget_values(validated_data, spark)
-    approvals.form_event_and_send_to_control(enriched_data, dbutils, spark) and displayHTML("<h1>Request sent.</h1>")    
 except Exception as e:
     if not "InputWidgetNotDefined" in str(e): print(e) 
 
 # COMMAND ----------
 
 from backend import approvals
+try:
+    if (data != None):
+        validated_data = approvals.validate_widget_values(data, dbutils, spark)
+        enriched_data = approvals.enrich_widget_values(validated_data, spark)
+        approvals.form_event_and_send_to_control(enriched_data, dbutils, spark) and displayHTML("<h1>Request sent.</h1>") 
+except Exception as e:
+    if not "InputWidgetNotDefined" in str(e): print(e) 
+
+# COMMAND ----------
+
 df = approvals.display_widgets(spark, dbutils)
 
 # COMMAND ----------
