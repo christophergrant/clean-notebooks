e5a47f3c2fdbf9a8256d8989eb5094e3c67c6998
angelamchu <angelamchu>
2022-06-06 14:14:16 +0000

    Added to force code list re-load

--- create_job.py
+++ create_job.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+dbutils.widgets.removeAll()
+
+# COMMAND ----------
+
 from backend import jobs
 jobs.display_widgets(spark, dbutils)
 
@@ -9,3 +13,7 @@ data = jobs.get_widget_values(dbutils)
 # COMMAND ----------
 
 jobs.process(data, spark, dbutils)
+
+# COMMAND ----------
+
+
