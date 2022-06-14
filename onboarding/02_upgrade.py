5c0d063945928e5b8fef536d0ef91a8531acf4ce
craig-db <craig-db>
2022-06-14 19:59:44 +0000

    separate file to make upgrade easy for client

---
+++ onboarding/02_upgrade.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# DBTITLE 1,For v1.1
+from delta.tables import DeltaTable
+
+code_table_columns = DeltaTable.forName(spark, "control.code").toDF().columns
+if 'default_parameters' not in code_table_columns:
+    alter_sql = f"ALTER TABLE control.code ADD COLUMNS (default_parameters string)"
+    print(f"adding new column via DDL: {alter_sql}")
+    spark.sql(alter_sql)
+else:
+    print("control.code already has the default_parameters column")
+
+# COMMAND ----------
+
+runs_table_columns = DeltaTable.forName(spark, "control.runs").toDF().columns
+if 'error_trace' not in runs_table_columns:
+    alter_sql = f"ALTER TABLE control.runs ADD COLUMNS (error STRING, error_trace STRING)"
+    print(f"adding new column via DDL: {alter_sql}")
+    spark.sql(alter_sql)
+else:
+    print("control.runs already has the error_trace column")
